dceccbef66cbaa40a540ffb9381b455e12cb5808
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-01-19 14:18:40 +0200

    3.15: tietokannasta poistaminen

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ const Person = require('./models/person')
 
 const app = express()
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
@@ -38,6 +15,8 @@ morgan.token('person', (req, res) => {
 })
 var morganLogger =  ':method :url :status :res[content-length] - :response-time ms :person'
 app.use(morgan(morganLogger))
+
+//============== ROUTES ===============
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(people => {
@@ -81,10 +60,12 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.res.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
